2c176019f01ac4ac4059719bd6603922ecd20b87
Poghos Barseghyan <bpoghos94@gmail.com>
2023-10-21 19:54:25 +0400

    task

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,15 +3,20 @@ import './main.css'
 
 export default function Main() {
     return (
-        <div className='main'>
-            <h1>Amazing Heading Text Right Here</h1>
-            <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
-            <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
-            <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
-            <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
-            <div className='button-box'>
-                <button className='button button1'>Download App</button>
-                <button className='button button2'>Download App</button>
+        <div className='main-box'>
+            <div className='main'>
+                <h1>Amazing Heading Text Right Here</h1>
+                <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
+                <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
+                <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
+                <p>dsghjdhsagjd dsjhgadhjsa hdgshjg hdsgahdj hsjhahd</p>
+                <div className='button-box'>
+                    <button className='button button1'>Download App</button>
+                    <button className='button button2'>Download App</button>
+                </div>
+            </div>
+            <div className='image'>
+
             </div>
         </div>
     )
