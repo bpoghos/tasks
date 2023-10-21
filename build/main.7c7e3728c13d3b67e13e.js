48f89436efe361752ea72a93e8e4baaed3c13ea5
Poghos Barseghyan <bpoghos94@gmail.com>
2023-10-21 13:31:22 +0400

    tasks

---
+++ build/main.7c7e3728c13d3b67e13e.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("// let age: number = 23\n// let str: string | number = 'test'\n// let t: boolean | number = false\n// let numarr: number[] = [2, 5, 10, 5]\n// let strarr: string[] = ['fhjsf', 'sdfjsd']\n// let booarr: boolean[] = [true, false]\n// let strnumarr: (number | string)[] = [2, 'fsds']\n// let obj: object = {\n//     name: 'jhon'\n// }\n// let objarr: object[] = [{}, {}, {}]\n// const arrarr: (string[] | number[])[] = [['sdsd', 'dbsjdb'], [1, 3, 7]]\n// function test(name: string): string {\n//     return 'hello, ' + name\n// }\n// const data: string = test('Jhon')\n// function counter(arg: number[]): number {\n//     const sum = arg.reduce((acc, cur) => acc + cur, 0)\n//     return sum / 2\n// }\n// const count: number = counter([2, 5, 7, 9, 1])\n// interface User {\n//     name: string;\n//     age: number;\n//     isAdmin?: boolean;\n//     esim: string[]\n//     hello: string\n// }\n// const user: User = {\n//     name: 'Jhon',\n//     age: 35,\n//     esim: ['sd', 'das'],\n//     hello: ''\n// }\n// user.isAdmin = false\n// interface Student {\n//     name: string;\n//     age: number\n// }\n// const students: Student[] = [\n//     { name: 'John', age: 20 },\n//     { name: 'Mike', age: 22 },\n//     { name: 'Piter', age: 19 },\n//     { name: 'Anna', age: 24 },\n// ]\n// function student(age: number): Student[] {\n//     return students.filter(student => student.age > age)\n// }\n// const result: Student[] = student(20);\n// function factorial(num: number): number {\n//     if (num === 0 || num === 1) {\n//         return 1;\n//     } else {\n//         return num * factorial(num - 1);\n//     }\n// }\n// const result: number = factorial(5) // 120\n\n\n//# sourceURL=webpack://tasks/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.ts
@@ -0,0 +1,91 @@
+// let age: number = 23
+// let str: string | number = 'test'
+// let t: boolean | number = false
+// let numarr: number[] = [2, 5, 10, 5]
+// let strarr: string[] = ['fhjsf', 'sdfjsd']
+// let booarr: boolean[] = [true, false]
+// let strnumarr: (number | string)[] = [2, 'fsds']
+// let obj: object = {
+//     name: 'jhon'
+// }
+// let objarr: object[] = [{}, {}, {}]
+// const arrarr: (string[] | number[])[] = [['sdsd', 'dbsjdb'], [1, 3, 7]]
+
+
+// function test(name: string): string {
+//     return 'hello, ' + name
+// }
+// const data: string = test('Jhon')
+
+
+
+// function counter(arg: number[]): number {
+//     const sum = arg.reduce((acc, cur) => acc + cur, 0)
+//     return sum / 2
+// }
+
+// const count: number = counter([2, 5, 7, 9, 1])
+
+
+// interface User {
+//     name: string;
+//     age: number;
+//     isAdmin?: boolean;
+//     esim: string[]
+//     hello: string
+// }
+
+// const user: User = {
+//     name: 'Jhon',
+//     age: 35,
+//     esim: ['sd', 'das'],
+//     hello: ''
+// }
+
+// user.isAdmin = false
+
+
+// interface Student {
+//     name: string;
+//     age: number
+// }
+
+
+// const students: Student[] = [
+//     { name: 'John', age: 20 },
+//     { name: 'Mike', age: 22 },
+//     { name: 'Piter', age: 19 },
+//     { name: 'Anna', age: 24 },
+// ]
+
+// function student(age: number): Student[] {
+//     return students.filter(student => student.age > age)
+
+// }
+// const result: Student[] = student(20);
+
+
+// function factorial(num: number): number {
+//     if (num === 0 || num === 1) {
+//         return 1;
+//     } else {
+//         return num * factorial(num - 1);
+//     }
+// }
+
+// const result: number = factorial(5) // 120
+
+
+// function randomNumbers(): number {
+//     const random: number = Math.random();
+//     const counter: number = random * (20 - 3) + 3;
+//     const randomNumber: number = Math.round(counter);
+
+//     return randomNumber;
+// }
+
+
+
+
+
+
